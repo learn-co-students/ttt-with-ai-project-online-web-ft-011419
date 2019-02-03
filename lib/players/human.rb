cecbb28bd3b2e7bde6a9be3519583e5236a451e0
Luke Granger <grangerl330@gmail.com>
2019-02-03 06:55:41 +0000

    Done.

---
+++ lib/players/human.rb
@@ -0,0 +1,15 @@
+require_relative '../player.rb'
+
+class Players
+
+  class Human < Player
+
+    def move(board)
+      puts "Please enter your move as a number 1-9"
+      user_input = gets.strip
+      user_input
+    end
+
+  end
+
+end
